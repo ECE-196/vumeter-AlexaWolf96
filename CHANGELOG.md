a73e3505f81d8ae83714a19952f47e21749365c6
AlexaWolf96 <anordstrom@ucsd.edu>
2024-04-22 17:22:32 -0700

    working vu-meter and code
    video of it working and the completed code

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# CHANGELOG
+
+## 04/22/2024
+### Contributors
+Alexa Nordstrom
+
+### Added
+Video of working vu meter in enclosure
+updated code in app.py
+### Changed
+...
+
+### Removed
+...
--- main.py
+++ main.py
@@ -13,10 +13,20 @@ led_pins = [
     board.IO21,
     board.IO26, # type: ignore
     board.IO47,
-    # do the rest...
+    board.IO33, 
+    board.IO34, 
+    board.IO48,
+    board.IO35,
+    board.IO36,
+    board.IO37,
+    board.IO38,
+    board.IO39,
 ]
 
 leds = [DigitalInOut(pin) for pin in led_pins]
+
+thresholds = [20000, 25000, 26000, 27000, 28000, 30000, 
+              40000, 45000, 50000, 51000, 52000]
 
 for led in leds:
     led.direction = Direction.OUTPUT
@@ -27,10 +37,16 @@ while True:
 
     print(volume)
 
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
+    leds_to_light = 0
+    for i, threshold in enumerate(thresholds):
+        if volume >= threshold:  # Check if volume exceeds the threshold
+            leds_to_light = i + 1  
 
-    sleep(1)
+    # Turn on/off LEDs
+    for i, led in enumerate(leds):
+        led.value = i < leds_to_light
+
+    sleep(0.1)
 
     # instead of blinking,
     # how can you make the LEDs
